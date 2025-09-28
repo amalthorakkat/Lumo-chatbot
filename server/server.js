25800bf157878520416db02bfd393620cc3f436c
amalthorakkat <amalthorakkat@gmail.com>
2025-09-28 16:26:22 +0530

    vercel render error fix tru-2

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ app.use(
       "http://localhost:5173",
       "https://lumo-chatbot-cju07m72f-amal-thorakkats-projects.vercel.app",
       "https://lumo-chatbot-git-main-amal-thorakkats-projects.vercel.app",
+      "https://lumo-chatbot-sigma.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
